b9372540fd32c12e77b519c51b7f734dfa112767
Zivorad Baralic <zivorad.baralic@sonymobile.com>
2019-04-15 16:13:37 +0200

    Adding project dependencies and util package
    Current dependencies for:
    - Rx
    - Lifecycle, LiveData and ViewModel
    - Dependency Injection Framework

---
+++ app/src/main/java/com/obaralic/shade/util/extension/RxExtensions.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2019 Obaralic.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.obaralic.shade.util.extension
+
+import io.reactivex.Maybe
+
+fun <T> maybe(value: T?): Maybe<T> {
+    return if(value == null) Maybe.empty()
+    else Maybe.just(value)
+}
